bb35fe3a6b91d7e2345bac80b9366f4f8f463240
posCmd <afolabiyasser06@gmail.com>
2023-02-01 14:56:58 +0000

    Finalisation du media-query de la tablette , debut du mobile

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const toggle_btn_Id = document.getElementById("toggle_btn");
+const close_btn_Id = document.getElementById("close_btn");
+
+const toggle_btn_Cl = document.querySelector(".toggle_btn");
+const close_btn_Cl = document.querySelector(".close_btn");
+
+
+
+const nav_menu = document.querySelector(".nav_menu");
+
+toggle_btn_Id.addEventListener('click', function(e){
+    e.preventDefault();
+    nav_menu.style.right = ".1%"
+    close_btn_Cl.style.display = "block";
+})
+
+close_btn_Id.addEventListener('click', function(e){
+    e.preventDefault();
+    nav_menu.style.right = "-100%"
+    close_btn_Cl.style.display = "none";
+    
+})
